b7c613245e724f1dd7d8cdda02525d402168d653
Paula Salino Ribeiro <paulasalinor@gmail.com>
2024-05-08 11:23:52 -0300

    feat: updated and delete invoices

---
+++ app/lib/actions.ts
@@ -0,0 +1,78 @@
+'use server';
+
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+// React Server Actions allow you to run asynchronous code directly on the server. 
+// They eliminate the need to create API endpoints to mutate your data. 
+// Instead, you write asynchronous functions that execute on the server and can be 
+// invoked from your Client or Server Components.
+// Security is a top priority for web applications, as they can be vulnerable to various threats.
+// This is where Server Actions come in. They offer an effective security solution, protecting 
+// against different types of attacks, securing your data, and ensuring authorized access. 
+// Server Actions achieve this through techniques like POST requests, encrypted closures, 
+// strict input checks, error message hashing, and host restrictions, all working together 
+// to significantly enhance your app's safety.
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+  // const rawFormData = {
+  //   customerId: formData.get('customerId'),
+  //   amount: formData.get('amount'),
+  //   status: formData.get('status'),
+  // };
+  // const rawFormData = Object.fromEntries(formData.entries())
+
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  revalidatePath('/dashboard/invoices'); //  you want to clear this cache and trigger a new request to the server.
+  redirect('/dashboard/invoices');
+}
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
+}
